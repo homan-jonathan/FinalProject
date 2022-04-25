3b77afef489696aaf09b467a40a9441d9a7981b5
GVMartin23 <MartinGV19@gcc.edu>
2022-04-25 12:23:59 -0400

    Worked on cribbage counting points methods, got it to work in some way

--- src/Cribbage.java
+++ src/Cribbage.java
@@ -1,5 +1,4 @@
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 public class Cribbage {
     private LinkedList<Card> playerHand;
@@ -24,7 +23,7 @@ public class Cribbage {
      * Runs a game of Cribbage until the winning conditions are met
      */
     public void gameLoop(){
-        while (playerScore<finalScore&&compScore<playerScore) {
+        while (playerScore<finalScore&&compScore<finalScore) {
             deck = new Deck();
             playerHand = new LinkedList<>(deck.dealHand(4));
             compHand = new LinkedList<>(deck.dealHand(4));
@@ -39,9 +38,9 @@ public class Cribbage {
 
 
         if(playerScore>=compScore) {
-            System.out.println("Player Wins");
+            System.out.println("Player Wins: " + playerScore + " to " + compScore);
         } else {
-            System.out.println("Computer Wins");
+            System.out.println("Computer Wins: " + compScore + " to " + playerScore);
         }
 
 
@@ -49,25 +48,56 @@ public class Cribbage {
 
     /**
      * count the points that are in a players hand
-     * @param hand
-     * @return
+     * @param hand hand to count points
+     * @return number of points counted
      */
     public int countPoints(LinkedList<Card> hand){
         int points = 0;
+        points+=countFlush(hand);
         points+=countPairs(hand);
-        return 1;
+
+
+        return points;
     }
 
+    /**
+     * counts the points from pairs
+     * @param hand hand to count points
+     * @return number of points counted
+     */
     private int countPairs(LinkedList<Card> hand) {
+        ArrayList<Card> cards = new ArrayList<>(hand);
         int points = 0;
-        Card compareCard = new Card(hand.remove(0));
-        for (Card card: hand) {
-            if (compareCard.sameValue(card)){
-                points+=2;
+        points+=countFlush(hand);
+        for (int i = 0; i<cards.size()-1;i++) {
+            Card compareCard = cards.get(i);
+            for (int j = i+1; j<cards.size(); j++) {
+                if(compareCard.sameValue(cards.get(j))) {
+                    points+=2;
+                }
             }
-            compareCard = hand.remove(0);
         }
 
         return points;
     }
+
+    /**
+     * Checks if flush and adds point if flush
+     * @param hand hand of Cards to check
+     * @return points if flush exists
+     */
+    private int countFlush(LinkedList<Card> hand) {
+        HashSet<String> suitSet = new HashSet<>();
+        for (Card c: hand) {
+            suitSet.add(c.getSuit());
+        }
+        if (suitSet.size()==1) {
+            return hand.size();
+        } else {
+            return 0;
+        }
+    }
+
+
+
 }
